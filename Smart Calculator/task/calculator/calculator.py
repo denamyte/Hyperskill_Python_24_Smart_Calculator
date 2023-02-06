1459c0c527744add2d421b16f1b470c3b13a11e8
denys <denamyte@gmail.com>
2023-02-06 15:12:04 +0200

    2023-02-06: Stage 3/7: finished

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,6 +1,8 @@
 def main():
     while True:
         match input().strip().split():
+            case ['/help']:
+                print('The program calculates the sum of numbers')
             case ['/exit']:
                 print('Bye!')
                 break
